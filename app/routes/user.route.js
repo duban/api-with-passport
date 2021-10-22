7f648070f955b2c36e7dc726902b25b316faa93b
duban <mikael.duban@gmail.com>
2021-10-23 00:05:46 +0700

    add route auth

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+
 const auth = require('../middleware/auth.middleware');
 const awaitHandlerFactory = require('../middleware/awaitHandlerFactory.middleware');
 const userController = require('../controllers/user.controller');
 
-router.get('/', auth(), awaitHandlerFactory(userController.getAllUsers));
-// router.get('/', userController.getAllUsers); // localhost:3000/api/v1/users
-router.post('/', userController.createUser); // localhost:3000/api/v1/users
+const authBearer = require('../middleware/auth').authenticateBearer
+const authLocal = require('../middleware/auth').authenticateLocal;
+// const { authenticateLocal } = require('../middleware/auth');
 
-router.post('/login', userController.userLogin); // localhost:3000/api/v1/users/login
+// router.get('/', auth(), awaitHandlerFactory(userController.getAllUsers));
+router.get('/', authBearer,userController.getAllUsers); // localhost:3000/api/v1/users
+// router.post('/', userController.createUser); // localhost:3000/api/v1/users
+
+router.post('/login', authLocal,function(req, res, next) {
+    res.send({
+        token: req.user,
+      });
+  });
+
+// router.post('/login',auth ,userController.userLogin); // localhost:3000/api/v1/users/login
 router.get('/id/:id', userController.getUserById); // localhost:3000/api/v1/users/username/julia
 
 router.get('/username/:username', userController.getUserByuserName); // localhost:3000/api/v1/users/username/julia
