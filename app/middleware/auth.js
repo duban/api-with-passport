4bbf76de14b483f8ec9b210fbf40235d99c9183e
duban <mikael.duban@gmail.com>
2021-10-23 00:03:19 +0700

    add middleware

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,13 +1,40 @@
 const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
-const UserModel = require('../model/model');
+const jwt = require('jwt-simple');
+const dotenv = require('dotenv');
+dotenv.config();
 
-passport.use(new BearerStrategy(
-    function(token, done) {
-      User.findOne({ token: token }, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        return done(null, user, { scope: 'all' });
-      });
+const LocalStrategy = require('passport-local').Strategy;
+const BearerStrategy = require('passport-http-bearer').Strategy;
+
+const ADMIN = 'admin';
+const ADMIN_PASSWORD = 'password';
+const SECRET = 'mysecret';
+
+function initialize(options) {
+  return passport.initialize(options);
+}
+
+passport.use(new BearerStrategy((token, done) => {
+  try {
+    const { username } = jwt.decode(token, SECRET);
+    if (username === ADMIN) {
+      done(null, username);
+      return;
     }
-  ));
+    done(null, false);
+  } catch (error) {
+    done(null, false);
+  }
+}));
+
+passport.use(new LocalStrategy((username, password, done) => {
+  if (username === ADMIN && password === ADMIN_PASSWORD) {
+    done(null, jwt.encode({ username }, SECRET));
+    return;
+  }
+  done(null, false);
+}));
+
+exports.initialize = initialize;
+exports.authenticateLocal = passport.authenticate('local', { session: false });
+exports.authenticateBearer = passport.authenticate('bearer', { session: false });
